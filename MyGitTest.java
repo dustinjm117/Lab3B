071ef52f5d8adbd1c3a0b3dc4a12c29700194222
Dustin McLean <djmclean@carroll.edu>
2018-10-10 14:15:59 -0600

    changed message in branchA

--- MyGitTest.java
+++ MyGitTest.java
@@ -3,7 +3,7 @@ public class MyGitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello Git Test");
+System.out.println("Dustin says Hello Git Test Branch AAAA");
 	}
 
 }
