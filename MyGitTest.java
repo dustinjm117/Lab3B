f596f4861c68977c6d82237c341a341fb0f7ba78
Dustin McLean <djmclean@carroll.edu>
2018-10-10 14:17:16 -0600

    Hello message fix 2

--- MyGitTest.java
+++ MyGitTest.java
@@ -3,7 +3,7 @@ public class MyGitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Dustin says Hello Git Test Branch AAAA");
+System.out.println("Dustin says Branch Git Test");
 	}
 
 }
